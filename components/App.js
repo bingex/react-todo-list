4bdd9452cffeeae9aee430685af7a5336be24ba8
bingex <r.datsyuk@c.elantix.net>
2016-08-02 00:37:01 +0300

    First commit

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TextInput from './TextInput';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App component</h1>
+                <TextInput />
+            </div>
+        )
+    }
+}
+
+export default App
